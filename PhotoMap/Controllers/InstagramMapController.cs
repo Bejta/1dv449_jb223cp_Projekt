39d07a2c10bbc3588a67321d17b4ab5c65faf384
Jasmin Bejtovic <jb223cp@student.lnu.se>
2016-02-01 00:06:03 +0100

    Some work in Webservice classes

---
+++ PhotoMap/Controllers/InstagramMapController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhotoMap.Controllers
+{
+    public class InstagramMapController : Controller
+    {
+        // GET: InstagramMap
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PhotoMap/Models/Webservices/OAuthInstagramWrapper.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace PhotoMap.Models.Webservices
+{
+    public class OAuthInstagramAccessToken
+    {
+        [JsonProperty("access_token")]
+        public string Token { get; set; }
+
+        [JsonProperty("token_type")]
+        public string Type { get; set; }
+    }
+    public class OAuthInstagramWrapper
+    {
+        public OAuthInstagramAccessToken GetAccessToken(){
+
+        // Get settings from Web.config.
+        var client_id = ConfigurationManager.AppSettings["instagram.clientid"];
+        var redirect_uri = ConfigurationManager.AppSettings["instagram.redirecturi"];
+        var oAuthUrl = ConfigurationManager.AppSettings["OAuthUrl"];
+
+        return accessToken;
+    }
+    }
+}
